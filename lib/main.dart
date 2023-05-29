d5ec9cceb8d76fbd5c9816d4835054d32d6ded67
halla <hallaalhasan0@gmail.com>
2023-05-29 13:27:55 +0300

    ny-times

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:ny_times1/bloc/theme_cubit.dart';
 import 'package:ny_times1/screens/homepage.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform );
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
   HydratedBloc.storage = await HydratedStorage.build(
       storageDirectory: await getApplicationDocumentsDirectory());
   await Future.delayed(const Duration(seconds: 2));
@@ -29,10 +34,13 @@ class MyApp extends StatelessWidget {
         builder: (context, state) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
+
             themeMode: state ? ThemeMode.light : ThemeMode.dark,
             darkTheme: ThemeData.dark(),
             theme:
-            _buildTheme(Brightness.light),
+             ThemeData(
+               fontFamily: GoogleFonts.lato().fontFamily,
+             ) ,
 
             home: const HomeScreen(),
 
@@ -42,13 +50,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-ThemeData _buildTheme(brightness) {
-  var baseTheme = ThemeData(brightness: brightness);
-
-  return baseTheme.copyWith(
-    textTheme: GoogleFonts.latoTextTheme(baseTheme.textTheme),
-  );
-}
-
-
-
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -23,29 +23,35 @@ class HomeScreen extends StatelessWidget {
         if (state.status == NewsStatus.success) {
           return CustomScrollView(
               slivers: [
-            SliverToBoxAdapter(
-             child: BlocBuilder<ThemeCubit, bool>(
-            builder: (context, state) {
-              return SwitchListTile(
-                value: state,
-                title: const Text('Theme'),
-                onChanged: (value) {
-                  BlocProvider.of<ThemeCubit>(context).toggleTheme(value: value);
-                },
-              );
-            },
-            ),
-          ),
             SliverAppBar(
+              actions: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.notifications_none_outlined, color: Colors.black,)),
+
+              ],
+
               expandedHeight: 250.0,
               flexibleSpace: FlexibleSpaceBar(
                 title: Text(""),
                 background: Image.network(
-                  'https://library.northwestu.edu/wp-content/uploads/2019/06/nytimes.png',
+                  'https://img.freepik.com/free-photo/top-view-old-french-newspaper-pieces_23-2149318857.jpg?w=1060&t=st=1685283474~exp=1685284074~hmac=b3570da209a25fb18c5fc5f71474d36a6a7a25d4e98b055e89dde6d79a9bea8d0',
                   fit: BoxFit.cover,
                 ),
               ),
             ),
+                SliverToBoxAdapter(
+                  child: BlocBuilder<ThemeCubit, bool>(
+                    builder: (context, state) {
+                      return SwitchListTile(
+                        title: Text("Breaking News", style:Theme.of(context).textTheme.bodyLarge?.copyWith(fontSize: 30,fontWeight:FontWeight.bold),),
+                        value: state,
+                        onChanged: (value) {
+                          BlocProvider.of<ThemeCubit>(context).toggleTheme(value: value);
+                        },
+
+                      );
+                    },
+                  ),
+                ),
             SliverList(delegate: SliverChildBuilderDelegate((context, index) {
               return Container(
                 decoration: const BoxDecoration(
@@ -55,26 +61,38 @@ class HomeScreen extends StatelessWidget {
                 padding: EdgeInsets.all(8.0),
                 child: Row(
                   children: [
-                    Expanded(flex: 2, child: Text(state.allResults?.results?[index].title ?? "")),
                     Expanded(
-                      child: Container(
-                        width: 100.0,
-                        height: 150.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                        ),
-                        child: ClipRect(
-                          child: Image.network(
-                            state.allResults?.results?[index].media.firstOrNull
-                                    ?.mediaMetadata.firstOrNull?.url ??
-                                "https://library.northwestu.edu/wp-content/uploads/2019/06/nytimes.png",
-                            fit: BoxFit.fill,
-                            height: 150.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+
+                        children: [
+                          Container(
                             width: 100.0,
+                            height: 150.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(15)),
+                            ),
+                            child: ClipRect(
+                              child: Image.network(
+                                state.allResults?.results?[index].media.firstOrNull
+                                        ?.mediaMetadata.firstOrNull?.url ??
+                                    "https://library.northwestu.edu/wp-content/uploads/2019/06/nytimes.png",
+                                fit: BoxFit.fill,
+                                height: 150.0,
+                                width: 100.0,
+                              ),
+                            ),
                           ),
-                        ),
+                        ],
+
                       ),
                     ),
+                    Expanded(flex: 2, child: Text(state.allResults?.results?[index].title ?? "",style:  Theme.of(context)
+                        .textTheme
+                        .bodyLarge
+                        ?.copyWith(fontSize: 20),
+                    ),),
                   ],
                 ),
               );
@@ -89,9 +107,29 @@ class HomeScreen extends StatelessWidget {
 
         }
 
-        return Center(child: CircularProgressIndicator());
+        return const Center(child: CircularProgressIndicator());
       },
     ),
+        bottomNavigationBar: Container(
+          margin: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            borderRadius:BorderRadius.circular(16),
+            color: Colors.blueGrey.withOpacity(0.5)
+          ),
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            selectedItemColor: Colors.white,
+
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.rss_feed),label: "Feed"),
+
+
+            ],
+          ),
+        ),
 )
     );
   }
